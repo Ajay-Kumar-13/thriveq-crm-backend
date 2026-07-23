3b61e998a9cdeab21f0bb86832fb35ed57b63c70
Ajay-Kumar-13 <apulleti@gitam.in>
2026-07-23 08:39:42 +0530

    ext-authz

---
+++ external-authortization/src/main/java/com/thriveq/crm/external_authortization/ExternalAuthortizationApplication.java
@@ -0,0 +1,13 @@
+package com.thriveq.crm.external_authortization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalAuthortizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalAuthortizationApplication.class, args);
+	}
+
+}
